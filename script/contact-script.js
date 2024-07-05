378aa196db0a06f140a39aa7d3e5353768ea18c4
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-07-05 16:40:11 -0300

    update: contact page footer section and create contact page header component

--- script/contact-script.js
+++ script/contact-script.js
@@ -8,6 +8,51 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+
+function show(anything) {
+  document.querySelector(".contactHeader__menu").value = anything;
+}
+
+let menu = document.querySelector(".contactHeader__menu");
+let items = document.querySelector(".contactHeader__list-div");
+let links = document.querySelectorAll(".contactHeader__link");
+let body = document.querySelector("body");
+
+function closeMenu(event) {
+  if (!items.contains(event.target) && !menu.contains(event.target)) {
+    items.classList.remove("open-menu");
+    menu.classList.remove("open-menu");
+    body.classList.remove("open-menu");
+    document.removeEventListener("click", closeMenu);
+  }
+}
+
+menu.onclick = function () {
+  menu.classList.toggle("open-menu");
+  items.classList.toggle("open-menu");
+  body.classList.toggle("open-menu");
+  items.removeAttribute("style");
+  document.addEventListener("click", closeMenu);
+};
+
+items.onclick = function (event) {
+  event.stopPropagation();
+};
+
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    items.classList.remove("open-menu");
+    menu.classList.remove("open-menu");
+    body.classList.remove("open-menu");
+  });
+});
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth < 768) {
+    items.style.transition = "none";
+  }
+});
+
 document
   .getElementById("contactNewsletterForm")
   .addEventListener("submit", function (event) {
